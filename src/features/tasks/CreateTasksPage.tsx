8026ce07f274d05b137eb204418b1512a37b1d1d
Bui Tien Dat <buitiendat2702200@gmail.com>
2025-07-24 02:27:52 +0700

    Add: TaskForm, CreateTaskPage

--- src/features/tasks/CreateTasksPage.tsx
+++ src/features/tasks/CreateTasksPage.tsx
@@ -2,8 +2,10 @@ import TaskForm from "../../components/TaskForm/index";
 
 export default function TaskPage() {
   return (
-    <div className="min-h-screen bg-gray-50 py-20 px-6">
-      <TaskForm />
+    <div className="min-h-screen bg-gray-100">
+      <div className="py-20 px-6">
+        <TaskForm />
+      </div>
     </div>
   );
 }
